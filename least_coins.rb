cb1eb4ae8d14860c52c1dae9c798ca3b5c49f613
MCPhillips19 <mpclaire72@gmail.com>
2018-07-26 13:02:11 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,25 @@
-#write out your code here
-
-def least_coins(cents)
-
-#Code your answer here!
-
+def least_coins
+  x= 66
+  least_coin = {}
+  q = []
+  d= []
+  n= []
+  z=[]
+  w= []
+  y=[]
+  
+  (x/25).to_i == q 
+  least_coin["quarters"]= q
+  x- (q*25)== y
+  (y/10).to_i == d
+  least_coin["dimes"]= d
+  y- (d*10) == w
+  w/5.to_i == n
+  least_coin["nickels"]= n
+  w-(n*5) == z
+  least_coin["pennies"]= z
 end
+ puts least_coins
+  
+  
+  
